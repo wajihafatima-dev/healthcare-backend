61ae63363f97985806fece9a662789de585e72dd
alim <wajihafatima689@gmail.com>
2025-10-19 17:35:29 +0500

    .

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,21 +1,29 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "doctor", "staff", "patient"], default: "staff" },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["admin", "doctor", "staff", "patient"], 
+    default: "staff"
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
